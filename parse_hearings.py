25bd32080315ac951c239149a526fc6fa3120584
Jacob Casper <jacob.thomas.casper@gmail.com>
2020-06-07 12:18:09 -0500

    Work on CaseDetail csv from stdin and write to stdout

--- parse_hearings.py
+++ parse_hearings.py
@@ -1,44 +1,22 @@
+import sys
 import csv
 import json
 import os
 from typing import List
 
-import click
-
 import hearing
 
 
-def get_ids_to_parse(filename: str) -> List[str]:
-    ids = []
-    with open(filename, newline="") as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            ids.append(row[0])
-    return ids
-
-
-@click.command()
-@click.argument(
-    "infile",
-    type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True),
-)
-@click.argument("outfile", type=click.File(mode="w"), default="result.json")
-@click.option("--output-mode", default="json")
-def parse_all(infile, outfile, output_mode):
+def parse_all():
     parsed_hearings = []
-    ids_to_parse = get_ids_to_parse(infile)
-    for id_to_parse in ids_to_parse:
+    for id_to_parse in (line.rstrip() for line in sys.stdin):
         new_hearing = hearing.fetch_parsed_hearing(id_to_parse)
         parsed_hearings.append(new_hearing)
     parsed_hearings = [ph for ph in parsed_hearings if ph is not None]
-    if output_mode == "json":
-        json.dump(parsed_hearings, outfile)
-    elif output_mode == "csv":
-        with open("result.csv", "w", newline="") as csv_file:
-            writer = csv.DictWriter(csv_file, list(parsed_hearings[0].keys()))
-            writer.writeheader()
-            for parsed_hearing in parsed_hearings:
-                writer.writerow(parsed_hearing)
+    writer = csv.DictWriter(sys.stdout, list(parsed_hearings[0].keys()))
+    writer.writeheader()
+    for parsed_hearing in parsed_hearings:
+        writer.writerow(parsed_hearing)
 
 
 if __name__ == "__main__":
--- tests/test_cli.py
+++
@@ -1,7 +0,0 @@
-import parse_hearings
-
-
-class TestCLI:
-    def test_get_ids_to_parse(self):
-        ids = parse_hearings.get_ids_to_parse(filename="test_input.csv")
-        assert ids == ["J1-CV-20-001590", "J2-CV-20-001839", "J4-CV-20-000198"]
