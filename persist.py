a724dd37780fbce8e9d57c6c7cc7d4360ff99e0e
Jacob Casper <jacob.thomas.casper@gmail.com>
2020-07-28 13:08:39 -0500

    Add unique constrains to prevent data duplication
    Insert or Replace statements weren't hitting any constraints due to a
    sequence on the primary key never conflicting on event and disposition
    tables. Unique constraints will need to be leveraged on tables to
    prevent duplicates in the future.

--- persist.py
+++ persist.py
@@ -51,15 +51,16 @@ def rest_case(case):
     )
     # TODO scrape all event types in a similar way (writs should be consolidated in)
     # Types should mirror the values from the HTML table headers, HR/ER/SE/etc.
-    for hearing in case["hearings"]:
+    for hearing_number, hearing in enumerate(case["hearings"]):
         curs.execute(
             """
             INSERT OR REPLACE INTO EVENT
-            (CASE_DETAIL_ID, DATE, TIME, OFFICER, RESULT, TYPE)
-            VALUES (?, ?, ?, ?, ?, 'HR')
+            (CASE_DETAIL_ID, EVENT_NUMBER, DATE, TIME, OFFICER, RESULT, TYPE)
+            VALUES (?, ?, ?, ?, ?, ?, 'HR')
             """,
             (
                 case["case_number"],
+                hearing_number,
                 hearing["hearing_date"],
                 hearing["hearing_time"],
                 hearing["hearing_officer"],
