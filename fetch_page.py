d0a09760d3802f28533def0810d37089287222b5
alpiazza13 <alexpiazza2000@gmail.com.com>
2020-10-26 17:41:37 -0400

    adds back sandbox option

--- fetch_page.py
+++ fetch_page.py
@@ -13,7 +13,7 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 # options.add_argument('--disable-gpu')
-# options.add_argument('--no-sandbox')
+options.add_argument('--no-sandbox')
 options.add_argument("window-size=1920,1080")
 options.headless = True
 
