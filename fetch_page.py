0e414d4307a84a62c226331b5e9197c9023c8347
Matt Carey <matt@authorityspoke.com>
2021-01-27 21:49:19 -0600

    use headless chromedriver

--- fetch_page.py
+++ fetch_page.py
@@ -15,6 +15,7 @@ from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.add_argument("--no-sandbox")
+options.add_argument("--headless")
 options.add_argument("window-size=1920,1080")
 options.headless = True
 
